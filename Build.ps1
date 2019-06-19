e05d750889b0f308709de60e7975cbb089e97985
DanielDziubecki <daniel.dziubecki@wp.pl>
2019-06-19 20:56:23 +0200

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -26,7 +26,7 @@ function Exec
 
 exec { & dotnet restore }
 
-Invoke-MSBuild
+exec { & dotnet build  .\WaitForDocker\WaitForDocker.csproj -c Release}
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)

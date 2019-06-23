102875053462fdb55dff0452d7177e2b84859b30
DanielDziubecki <daniel.dziiubecki@wp.pl>
2019-06-23 21:05:10 +0200

    xunit logging

---
+++ WaitForDocker.Tests/DockerFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WaitForDocker.Integrations.Xunit;
+using Xunit.Abstractions;
+
+namespace WaitForDocker.Tests
+{
+    public class DockerFixture : IDisposable
+    {
+        private readonly XunitMessageSinkLogger xunitLogger;
+
+        public DockerFixture(IMessageSink output)
+        {
+            xunitLogger = new XunitMessageSinkLogger(output);
+            WaitForDocker.Compose(config => config.Logger = xunitLogger).GetAwaiter().GetResult();
+        }
+
+        public void Dispose()
+        {
+            WaitForDocker.ComposeKill(config => config.Logger = xunitLogger).GetAwaiter().GetResult();
+        }
+    }
+}
--- WaitForDocker.Tests/Tests.cs
+++ WaitForDocker.Tests/Tests.cs
@@ -1,26 +1,27 @@
 ﻿using System.Threading.Tasks;
 using WaitForDocker.Integrations.Xunit;
-using WaitForDocker.Logger;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace WaitForDocker.Tests
 {
+    [Collection(XunitConstants.DockerCollection)]
     public class Tests
     {
         private readonly ITestOutputHelper output;
 
         public Tests(ITestOutputHelper output)
         {
-            this.output = output;
-            ILogger logger;
-            WaitForDocker.Compose(config => { logger = config.Logger; }).GetAwaiter().GetResult();
+          // this.output = output;
+          // ILogger logger = new DefaultLogger();
+          // WaitForDocker.Compose(config => { config.Logger = logger; }).GetAwaiter().GetResult();
         }
 
         [Fact]
         public async Task IntegrationTest()
         {
-            await WaitForDocker.ComposeKill(config => { config.Logger = new XunitLogger(output); });
+            Assert.True(true);
+           // await WaitForDocker.ComposeKill(config => { config.Logger = new XunitLogger(output); });
         }
     }
 }
